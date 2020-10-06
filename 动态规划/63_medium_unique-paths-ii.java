b25256f0db395df6a839c9464c0c33da2b670f91
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-10-06 16:45:06 +0800

    Update 63_medium_unique-paths-ii.java

--- 动态规划/63_medium_unique-paths-ii.java
+++ 动态规划/63_medium_unique-paths-ii.java
@@ -18,3 +18,74 @@ https://leetcode.com/problems/unique-paths-ii/
 1. 向右 -> 向右 -> 向下 -> 向下
 2. 向下 -> 向下 -> 向右 -> 向右
 */
+
+/*
+解法：障碍物对应位置，路径数为0，其余计算与62题相同
+执行用时：1 ms, 在所有 Java 提交中击败了30.41% 的用户
+内存消耗：36.8 MB, 在所有 Java 提交中击败了90.41% 的用户
+*/
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] res = new int[m][n];
+        // 初始化
+        for(int i = 0; i < m; i++) {
+            if(obstacleGrid[i][0] == 1) {
+                for(int j = i; j < m; j++)
+                    res[j][0] = 0;
+                break;
+            }
+            else
+                res[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++) {
+            if(obstacleGrid[0][i] == 1) {
+                for(int j = i; j < n; j++)
+                    res[0][j] = 0;
+                break;
+            }
+            else
+                res[0][i] = 1;
+        }
+        // 迭代
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++) {
+                if(obstacleGrid[i][j] == 1)
+                    res[i][j] = 0;
+                else
+                    res[i][j] = res[i-1][j] + res[i][j-1];
+            }
+        return res[m-1][n-1];
+    }
+}
+
+/*
+空间优化：同样只需要一个数组，但需要注意的是，在62题中，res[0]一直都是1；如果在第一列出现了障碍物，对于的res[0]就是0，就不是1了。
+执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+内存消耗：36.9 MB, 在所有 Java 提交中击败了82.28% 的用户
+*/
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[] res = new int[n];
+        // 初始化，一旦遇到障碍物，就保持初始默认值0
+        for(int i = 0; i < n; i++) {
+            if(obstacleGrid[0][i] == 0)
+                res[i] = 1;
+            else
+                break;
+        }
+        for(int i = 1; i < m; i++) {
+            if(obstacleGrid[i][0] == 1)  // 如果第一列有障碍物，那么后续res的第一项均为0
+                res[0] = 0;      
+            for(int j = 1; j < n; j++) {
+                if(obstacleGrid[i][j] == 1)
+                    res[j] = 0;
+                else
+                    res[j] += res[j-1];
+            }
+        }
+        return res[n-1];
+    }
+}
