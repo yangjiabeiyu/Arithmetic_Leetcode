a499eeb6ae6e603def1df53852b6ab246adb544a
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-10-07 14:07:37 +0800

    Update 136_easy_single-number.java

--- 数组与链表/136_easy_single-number.java
+++ 数组与链表/136_easy_single-number.java
@@ -11,21 +11,16 @@ https://leetcode-cn.com/problems/single-number/
 /*
 解法一：使用额外空间，遍历数组，放入map中，对出现一次和出现两次的元素进行区分
 执行用时：15 ms, 在所有 Java 提交中击败了7.18% 的用户
-内存消耗：40.2 MB, 在所有 Java 提交中击败了5.26% 的用户
+内存消耗：40 MB, 在所有 Java 提交中击败了21.98% 的用户
 */
 class Solution {
     public int singleNumber(int[] nums) {
         HashMap<Integer, Boolean> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) {
-            if(map.containsKey(nums[i]))
-                map.put(nums[i], true);
-            else
-                map.put(nums[i], false);
-        }
-        for(int i : map.keySet()) {
+        for(int i = 0; i < nums.length; i++)
+            map.put(nums[i], map.containsKey(nums[i]));
+        for(int i : map.keySet())
             if(!map.get(i))
                 return i;
-        }
         return 0;
     }
 }
