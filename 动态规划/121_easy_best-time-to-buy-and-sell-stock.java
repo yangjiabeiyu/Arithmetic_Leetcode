a072f547fc5c952a0386f4bc583d2a7c62bd20aa
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-10-08 10:29:37 +0800

    Update 121_easy_best-time-to-buy-and-sell-stock.java

--- 动态规划/121_easy_best-time-to-buy-and-sell-stock.java
+++ 动态规划/121_easy_best-time-to-buy-and-sell-stock.java
@@ -14,7 +14,7 @@ https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
 /*
 解法一：动态规划
 执行用时：2 ms, 在所有 Java 提交中击败了63.60% 的用户
-内存消耗：38.7 MB, 在所有 Java 提交中击败了63.86% 的用户
+内存消耗：38.6 MB, 在所有 Java 提交中击败了84.00% 的用户
 */
 class Solution {
     public int maxProfit(int[] prices) {
@@ -25,7 +25,6 @@ class Solution {
             return 0;
         int profit = 0;
         int[] dp = new int[prices.length];
-        dp[0] = 0;
         for(int i = 1; i < prices.length; i++) {
             dp[i] = Math.max(0, dp[i-1]+prices[i]-prices[i-1]);
             profit = Math.max(profit, dp[i]);
@@ -34,3 +33,34 @@ class Solution {
     }
 }
 
+/*
+空间优化
+执行用时：2 ms, 在所有 Java 提交中击败了63.60% 的用户
+内存消耗：38.9 MB, 在所有 Java 提交中击败了39.66% 的用户
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, cur = 0;
+        for(int i = 1; i < prices.length; i++) {
+            cur = Math.max(0, cur+prices[i]-prices[i-1]);
+            profit = Math.max(profit, cur);
+        }
+        return profit;
+    }
+}
+
+/*
+解法二：双指针，一个记录最小值，一个刷新最大差值
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0)
+            return 0;
+        int minValue = prices[0], profit = 0;
+        for(int i = 1; i < prices.length; i++) {
+            profit = Math.max(profit, prices[i] - minValue);
+            minValue = Math.min(minValue, prices[i]);
+        }
+        return profit;
+    }
+}
