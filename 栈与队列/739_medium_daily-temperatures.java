52d4ac88670d6d80424d8c0e8c46ea16ff908c3f
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-10-13 22:00:25 +0800

    Create 739_medium_daily-temperatures.java

---
+++ 栈与队列/739_medium_daily-temperatures.java
@@ -0,0 +1,29 @@
+/*
+每日温度
+https://leetcode-cn.com/problems/daily-temperatures/
+题目描述
+请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+示例
+给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+*/
+
+/*
+解法：单调栈
+执行用时：23 ms, 在所有 Java 提交中击败了45.73% 的用户
+内存消耗：46.6 MB, 在所有 Java 提交中击败了48.20% 的用户
+*/
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[T.length];
+        for(int i = 0; i < T.length; i++) {
+            while(!stack.isEmpty() && T[stack.peek()] < T[i]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
