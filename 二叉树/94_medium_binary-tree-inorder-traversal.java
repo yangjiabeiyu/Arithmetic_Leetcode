1b925e7666108f608b7225effc2d761f4ce5c8cd
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-10-18 13:37:21 +0800

    Create 94_medium_binary-tree-inorder-traversal.java

---
+++ 二叉树/94_medium_binary-tree-inorder-traversal.java
@@ -0,0 +1,53 @@
+/*
+二叉树的中序遍历
+https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+*/
+
+/*
+解法一：递归
+执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+内存消耗：36.5 MB, 在所有 Java 提交中击败了99.42% 的用户
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        fun(root, list);
+        return list;
+    }
+    public void fun(TreeNode node, List list) {
+        if(node == null)
+            return;
+        fun(node.left, list);
+        list.add(node.val);
+        fun(node.right, list);
+    }
+}
