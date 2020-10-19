3e9290630669447f014fd7efd9ace63ac5a8a8dc
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-10-19 19:24:29 +0800

    Create 27_easy_二叉树的镜像.java

---
+++ 剑指offer/27_easy_二叉树的镜像.java
@@ -0,0 +1,53 @@
+/*
+二叉树的镜像
+请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+例如输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+镜像输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+示例 1：
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/*
+解法：递归
+执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+内存消耗：35.8 MB, 在所有 Java 提交中击败了98.47% 的用户
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null)
+            return root;
+        TreeNode temp = mirrorTree(root.right);
+        root.right = mirrorTree(root.left);
+        root.left = temp;
+        return root;
+    }
+}
+
