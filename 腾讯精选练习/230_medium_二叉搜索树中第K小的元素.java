85613b0b90d09d8b3d70749ad6e58ae7482ccde3
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-10-28 22:15:51 +0800

    Create 230_medium_二叉搜索树中第K小的元素.java

---
+++ 腾讯精选练习/230_medium_二叉搜索树中第K小的元素.java
@@ -0,0 +1,80 @@
+/*
+230. 二叉搜索树中第K小的元素
+
+给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+
+说明：
+你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+
+示例 1:
+输入: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+输出: 1
+
+示例 2:
+输入: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+输出: 3
+进阶：
+如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+*/
+
+/*
+解法：迭代
+执行用时：1 ms, 在所有 Java 提交中击败了55.09% 的用户
+内存消耗：38.1 MB, 在所有 Java 提交中击败了96.11% 的用户
+*/
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while(!stack.isEmpty() || root != null) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            k--;
+            if(k == 0)
+                break;
+            root = root.right;
+        }
+        return root.val;
+    }
+}
+
+/*
+解法二：递归
+执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+内存消耗：38.3 MB, 在所有 Java 提交中击败了91.92% 的用户
+*/
+class Solution {
+    public int k = 0;
+    public int res = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        kthSmallest(root);
+        return res;
+    }
+    public void kthSmallest(TreeNode root) {
+        if(root == null)
+            return;
+        kthSmallest(root.left);
+        if(--k == 0) {
+            res = root.val;
+            return;
+        }
+        kthSmallest(root.right);
+    }
+}
+
+
