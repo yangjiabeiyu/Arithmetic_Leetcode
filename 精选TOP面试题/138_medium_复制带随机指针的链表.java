b806f3b753817a83d95b82735bcae2f9fcf03e2d
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-11-04 21:35:00 +0800

    Create 138_medium_复制带随机指针的链表.java

---
+++ 精选TOP面试题/138_medium_复制带随机指针的链表.java
@@ -0,0 +1,60 @@
+/*
+138. 复制带随机指针的链表
+https://leetcode-cn.com/problems/copy-list-with-random-pointer/
+给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+要求返回这个链表的 深拷贝。 
+我们用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+    val：一个表示 Node.val 的整数。
+    random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+*/
+
+/*
+解法一：使用hashmap存储老新节点，这样可以通过老节点的random指向的节点，得到对应的新节点。
+执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+内存消耗：38.4 MB, 在所有 Java 提交中击败了75.15% 的用户
+*/
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Node headB = new Node(head.val), p = head, q = headB;
+        map.put(p, q);
+        while(p.next != null) {
+            p = p.next;
+            q.next = new Node(p.val);
+            map.put(p, q.next);            // 放入新老节点
+            q = q.next;
+        }
+        p = head;
+        q = headB;
+        while(p != null) {
+            q.random = map.get(p.random);   // 通过map得到对应的指向
+            p = p.next;
+            q = q.next;
+        }
+        return headB;
+    }
+}
+
+/*
+解法二：把新的节点放在后面，这样找random，直接next就是。
+
+*/
+
+
+
