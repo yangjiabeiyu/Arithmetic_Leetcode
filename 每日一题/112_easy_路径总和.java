0a04d2b8c4397e6a58d4530479ff49ff28d54af6
yangjiabeiyu <41142298+yangjiabeiyu@users.noreply.github.com>
2020-11-16 11:47:34 +0800

    Create 112_easy_路径总和.java

---
+++ 每日一题/112_easy_路径总和.java
@@ -0,0 +1,33 @@
+/*
+112. 路径总和
+https://leetcode-cn.com/problems/path-sum/
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+说明: 叶子节点是指没有子节点的节点。
+
+示例: 
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+
+返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+*/
+
+/*
+解法：递归即可
+执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+内存消耗：37.9 MB, 在所有 Java 提交中击败了99.30% 的用户
+*/
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null) return sum == root.val;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+
